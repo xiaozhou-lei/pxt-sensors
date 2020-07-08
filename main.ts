962fb839315e52b2d0d7a6228033f5b17a80e25d
xiaozhou-lei <61865102+xiaozhou-lei@users.noreply.github.com>
2020-07-08 19:53:12 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -471,152 +471,150 @@ namespace sensors {
 	 *  RGB超声波
 	 */
 	
-	const LED0_ON_L = 0x06
-	const LED0_ON_H = 0x07
-	const LED0_OFF_L = 0x08
-	const LED0_OFF_H = 0x09
-	const ALL_LED_ON_L = 0xFA
-	const ALL_LED_ON_H = 0xFB
-	const ALL_LED_OFF_L = 0xFC
-	const ALL_LED_OFF_H = 0xFD
+// 	const LED0_ON_L = 0x06
+// 	const LED0_ON_H = 0x07
+// 	const LED0_OFF_L = 0x08
+// 	const LED0_OFF_H = 0x09
+// 	const ALL_LED_ON_L = 0xFA
+// 	const ALL_LED_ON_H = 0xFB
+// 	const ALL_LED_OFF_L = 0xFC
+// 	const ALL_LED_OFF_H = 0xFD
 	
-	export enum SonarVersion {
-	V1 = 0x1,
-	V2 = 0x2
-	}
+// 	export enum SonarVersion {
+// 	V1 = 0x1,
+// 	V2 = 0x2
+// 	}
 	
-	let initialized = false
-	let neoStrip: neopixel.Strip;
-	let matBuf = pins.createBuffer(17);
-	let distanceBuf = 0;
+// 	let initialized = false
+// 	let neoStrip: neopixel.Strip;
+// 	let matBuf = pins.createBuffer(17);
+// 	let distanceBuf = 0;
 	
 	
-	/**
-	 * Get RUS04 distance
-	 * @param pin Microbit ultrasonic pin; eg: P2
-	*/
-	//% blockId=motorbit_ultrasonic block="Read RgbUltrasonic Distance|pin %pin|cm"  group="RGB超声波"
-	//% weight=76
-	export function Ultrasonic(pin: DigitalPin): number {
-		return UltrasonicVer(pin, SonarVersion.V1);
-	}
+// 	/**
+// 	 * Get RUS04 distance
+// 	 * @param pin Microbit ultrasonic pin; eg: P2
+// 	*/
+// 	//% blockId=motorbit_ultrasonic block="Read RgbUltrasonic Distance|pin %pin|cm"  group="RGB超声波"
+// 	//% weight=76
+// 	export function Ultrasonic(pin: DigitalPin): number {
+// 		return UltrasonicVer(pin, SonarVersion.V1);
+// 	}
 
-	function UltrasonicVer(pin: DigitalPin, v: SonarVersion): number {
+// 	function UltrasonicVer(pin: DigitalPin, v: SonarVersion): number {
 
-		// send pulse
-		if (v == SonarVersion.V1) {
-			pins.setPull(pin, PinPullMode.PullNone);
-		}
-		else { pins.setPull(pin, PinPullMode.PullDown); }
-		pins.digitalWritePin(pin, 0);
-		control.waitMicros(2);
-		pins.digitalWritePin(pin, 1);
-		control.waitMicros(50);
-		pins.digitalWritePin(pin, 0);
+// 		// send pulse
+// 		if (v == SonarVersion.V1) {
+// 			pins.setPull(pin, PinPullMode.PullNone);
+// 		}
+// 		else { pins.setPull(pin, PinPullMode.PullDown); }
+// 		pins.digitalWritePin(pin, 0);
+// 		control.waitMicros(2);
+// 		pins.digitalWritePin(pin, 1);
+// 		control.waitMicros(50);
+// 		pins.digitalWritePin(pin, 0);
 
-		// read pulse
-		let d = pins.pulseIn(pin, PulseValue.High, 25000);
-		let ret = d;
-		// filter timeout spikes
-		if (ret == 0 && distanceBuf != 0) {
-			ret = distanceBuf;
-		}
-		distanceBuf = d;
-		if (v == SonarVersion.V1) {
-			return Math.floor(ret * 9 / 6 / 58);
-		}
-		return Math.floor(ret / 40 + (ret / 800));
-		// Correction
-	}
+// 		// read pulse
+// 		let d = pins.pulseIn(pin, PulseValue.High, 25000);
+// 		let ret = d;
+// 		// filter timeout spikes
+// 		if (ret == 0 && distanceBuf != 0) {
+// 			ret = distanceBuf;
+// 		}
+// 		distanceBuf = d;
+// 		if (v == SonarVersion.V1) {
+// 			return Math.floor(ret * 9 / 6 / 58);
+// 		}
+// 		return Math.floor(ret / 40 + (ret / 800));
+// 		// Correction
+// 	}
 
-	function RgbDisplay(indexstart: number, indexend: number, rgb: RgbColors): void {
-		for (let i = indexstart; i <= indexend; i++) {
-			neoStrip.setPixelColor(i, rgb);
-		}
-		neoStrip.show();
-	}
+// 	function RgbDisplay(indexstart: number, indexend: number, rgb: RgbColors): void {
+// 		for (let i = indexstart; i <= indexend; i++) {
+// 			neoStrip.setPixelColor(i, rgb);
+// 		}
+// 		neoStrip.show();
+// 	}
 
-	//% blockId="motorbit_rus04" block="part %index|show color %rgb|effect %effect|rgbpin %pin"  group="RGB超声波"
-	//% weight=75
-	export function RUS_04(index: RgbUltrasonics, rgb: RgbColors, effect: ColorEffect,pin:DigitalPin): void {
-		let start, end;
-		if (!neoStrip) {
-			neoStrip = neopixel.create(pin, 6, NeoPixelMode.RGB)
-		}
-		if (index == RgbUltrasonics.Left) {
-			start = 0;
-			end = 2;
-		} else if (index == RgbUltrasonics.Right) {
-			start = 3;
-			end = 5;
-		} else if (index == RgbUltrasonics.All) {
-			start = 0;
-			end = 5;
-		}
-		switch(effect) {
-			case ColorEffect.None:
-				RgbDisplay(start, end, rgb);
-				break;
-			case ColorEffect.Breathing:
-			for (let i = 0; i < 255; i+=2) {
-				neoStrip.setBrightness(i);
-				RgbDisplay(start, end, rgb);
-				//basic.pause((255 - i)/2);
-				basic.pause((i < 20)? 80 :(255/i));
-			}
-			for (let i = 255; i > 0; i-=2) {
-				neoStrip.setBrightness(i);
-				RgbDisplay(start, end, rgb);
-				basic.pause((i < 20)? 80 :(255/i));
-			}
-			break;
-			case ColorEffect.Rotate:
-				for (let i = 0; i < 4; i++) {
-					neoStrip.setPixelColor(start, rgb);
-					neoStrip.setPixelColor(start+1, 0);
-					neoStrip.setPixelColor(start+2, 0);
-					if (index == RgbUltrasonics.All) {
-						neoStrip.setPixelColor(end-2, rgb);
-						neoStrip.setPixelColor(end-1, 0);
-						neoStrip.setPixelColor(end, 0);
-					}
-					neoStrip.show();
-					basic.pause(150);
-					neoStrip.setPixelColor(start, 0);
-					neoStrip.setPixelColor(start+1, rgb);
-					neoStrip.setPixelColor(start+2, 0);
-					if (index == RgbUltrasonics.All) {
-						neoStrip.setPixelColor(end-2, 0);
-						neoStrip.setPixelColor(end-1, rgb);
-						neoStrip.setPixelColor(end, 0);
-					}
-					neoStrip.show();
-					basic.pause(150);
-					neoStrip.setPixelColor(start, 0);
-					neoStrip.setPixelColor(start+1, 0);
-					neoStrip.setPixelColor(start+2, rgb);
-					if (index == RgbUltrasonics.All) {
-						neoStrip.setPixelColor(end-2, 0);
-						neoStrip.setPixelColor(end-1, 0);
-						neoStrip.setPixelColor(end, rgb);
-					}
-					neoStrip.show();
-					basic.pause(150);
-				}
-				RgbDisplay(4, 9, 0);
-				break;
-			case ColorEffect.Flash:
-			for (let i = 0; i < 6; i++) {
-				RgbDisplay(start, end, rgb);
-				basic.pause(150);
-				RgbDisplay(start, end, 0);
-				basic.pause(150);
-			}
-			break;
-		}
-	}
-
-
+// 	//% blockId="motorbit_rus04" block="part %index|show color %rgb|effect %effect|rgbpin %pin"  group="RGB超声波"
+// 	//% weight=75
+// 	export function RUS_04(index: RgbUltrasonics, rgb: RgbColors, effect: ColorEffect,pin:DigitalPin): void {
+// 		let start, end;
+// 		if (!neoStrip) {
+// 			neoStrip = neopixel.create(pin, 6, NeoPixelMode.RGB)
+// 		}
+// 		if (index == RgbUltrasonics.Left) {
+// 			start = 0;
+// 			end = 2;
+// 		} else if (index == RgbUltrasonics.Right) {
+// 			start = 3;
+// 			end = 5;
+// 		} else if (index == RgbUltrasonics.All) {
+// 			start = 0;
+// 			end = 5;
+// 		}
+// 		switch(effect) {
+// 			case ColorEffect.None:
+// 				RgbDisplay(start, end, rgb);
+// 				break;
+// 			case ColorEffect.Breathing:
+// 			for (let i = 0; i < 255; i+=2) {
+// 				neoStrip.setBrightness(i);
+// 				RgbDisplay(start, end, rgb);
+// 				//basic.pause((255 - i)/2);
+// 				basic.pause((i < 20)? 80 :(255/i));
+// 			}
+// 			for (let i = 255; i > 0; i-=2) {
+// 				neoStrip.setBrightness(i);
+// 				RgbDisplay(start, end, rgb);
+// 				basic.pause((i < 20)? 80 :(255/i));
+// 			}
+// 			break;
+// 			case ColorEffect.Rotate:
+// 				for (let i = 0; i < 4; i++) {
+// 					neoStrip.setPixelColor(start, rgb);
+// 					neoStrip.setPixelColor(start+1, 0);
+// 					neoStrip.setPixelColor(start+2, 0);
+// 					if (index == RgbUltrasonics.All) {
+// 						neoStrip.setPixelColor(end-2, rgb);
+// 						neoStrip.setPixelColor(end-1, 0);
+// 						neoStrip.setPixelColor(end, 0);
+// 					}
+// 					neoStrip.show();
+// 					basic.pause(150);
+// 					neoStrip.setPixelColor(start, 0);
+// 					neoStrip.setPixelColor(start+1, rgb);
+// 					neoStrip.setPixelColor(start+2, 0);
+// 					if (index == RgbUltrasonics.All) {
+// 						neoStrip.setPixelColor(end-2, 0);
+// 						neoStrip.setPixelColor(end-1, rgb);
+// 						neoStrip.setPixelColor(end, 0);
+// 					}
+// 					neoStrip.show();
+// 					basic.pause(150);
+// 					neoStrip.setPixelColor(start, 0);
+// 					neoStrip.setPixelColor(start+1, 0);
+// 					neoStrip.setPixelColor(start+2, rgb);
+// 					if (index == RgbUltrasonics.All) {
+// 						neoStrip.setPixelColor(end-2, 0);
+// 						neoStrip.setPixelColor(end-1, 0);
+// 						neoStrip.setPixelColor(end, rgb);
+// 					}
+// 					neoStrip.show();
+// 					basic.pause(150);
+// 				}
+// 				RgbDisplay(4, 9, 0);
+// 				break;
+// 			case ColorEffect.Flash:
+// 			for (let i = 0; i < 6; i++) {
+// 				RgbDisplay(start, end, rgb);
+// 				basic.pause(150);
+// 				RgbDisplay(start, end, 0);
+// 				basic.pause(150);
+// 			}
+// 			break;
+// 		}
+// 	}
 
 
 
@@ -629,62 +627,64 @@ namespace sensors {
 
 
 
-	/**
-	 * 温湿度传感器 
-	 */
 
-	//% blockId="readdht11" block="value of dht11 %dht11type| at pin %dht11pin"  group="温湿度传感器"
-	export function dht11value(dht11type: DHT11Type, dht11pin: DigitalPin): number {
 
-		pins.digitalWritePin(dht11pin, 0)
-		basic.pause(18)
-		let i = pins.digitalReadPin(dht11pin)
-		pins.setPull(dht11pin, PinPullMode.PullUp);
-		switch (dht11type) {
-			case 0:
-				let dhtvalue1 = 0;
-				let dhtcounter1 = 0;
-				while (pins.digitalReadPin(dht11pin) == 1);
-				while (pins.digitalReadPin(dht11pin) == 0);
-				while (pins.digitalReadPin(dht11pin) == 1);
-				for (let i = 0; i <= 32 - 1; i++) {
-					while (pins.digitalReadPin(dht11pin) == 0);
-					dhtcounter1 = 0
-					while (pins.digitalReadPin(dht11pin) == 1) {
-						dhtcounter1 += 1;
-					}
-					if (i > 15) {
-						if (dhtcounter1 > 2) {
-							dhtvalue1 = dhtvalue1 + (1 << (31 - i));
-						}
-					}
-				}
-				return ((dhtvalue1 & 0x0000ff00) >> 8);
-				break;
+// 	/**
+// 	 * 温湿度传感器 
+// 	 */
 
-			case 1:
-				while (pins.digitalReadPin(dht11pin) == 1);
-				while (pins.digitalReadPin(dht11pin) == 0);
-				while (pins.digitalReadPin(dht11pin) == 1);
+// 	//% blockId="readdht11" block="value of dht11 %dht11type| at pin %dht11pin"  group="温湿度传感器"
+// 	export function dht11value(dht11type: DHT11Type, dht11pin: DigitalPin): number {
 
-				let value = 0;
-				let counter = 0;
+// 		pins.digitalWritePin(dht11pin, 0)
+// 		basic.pause(18)
+// 		let i = pins.digitalReadPin(dht11pin)
+// 		pins.setPull(dht11pin, PinPullMode.PullUp);
+// 		switch (dht11type) {
+// 			case 0:
+// 				let dhtvalue1 = 0;
+// 				let dhtcounter1 = 0;
+// 				while (pins.digitalReadPin(dht11pin) == 1);
+// 				while (pins.digitalReadPin(dht11pin) == 0);
+// 				while (pins.digitalReadPin(dht11pin) == 1);
+// 				for (let i = 0; i <= 32 - 1; i++) {
+// 					while (pins.digitalReadPin(dht11pin) == 0);
+// 					dhtcounter1 = 0
+// 					while (pins.digitalReadPin(dht11pin) == 1) {
+// 						dhtcounter1 += 1;
+// 					}
+// 					if (i > 15) {
+// 						if (dhtcounter1 > 2) {
+// 							dhtvalue1 = dhtvalue1 + (1 << (31 - i));
+// 						}
+// 					}
+// 				}
+// 				return ((dhtvalue1 & 0x0000ff00) >> 8);
+// 				break;
 
-				for (let i = 0; i <= 8 - 1; i++) {
-					while (pins.digitalReadPin(dht11pin) == 0);
-					counter = 0
-					while (pins.digitalReadPin(dht11pin) == 1) {
-						counter += 1;
-					}
-					if (counter > 3) {
-						value = value + (1 << (7 - i));
-					}
-				}
-				return value;
-			default:
-				return 0;
-		}
-	}
+// 			case 1:
+// 				while (pins.digitalReadPin(dht11pin) == 1);
+// 				while (pins.digitalReadPin(dht11pin) == 0);
+// 				while (pins.digitalReadPin(dht11pin) == 1);
+
+// 				let value = 0;
+// 				let counter = 0;
+
+// 				for (let i = 0; i <= 8 - 1; i++) {
+// 					while (pins.digitalReadPin(dht11pin) == 0);
+// 					counter = 0
+// 					while (pins.digitalReadPin(dht11pin) == 1) {
+// 						counter += 1;
+// 					}
+// 					if (counter > 3) {
+// 						value = value + (1 << (7 - i));
+// 					}
+// 				}
+// 				return value;
+// 			default:
+// 				return 0;
+// 		}
+// 	}
 
 
 }
